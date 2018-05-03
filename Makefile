52c81626e5063960f7c03384e2704906a54fed5d
jonwright <wright@esrf.fr>
2018-05-03 12:51:05 +0200

    add zlib

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 
 _imgtrans.so : _imgtrans.c
-	gcc -shared -o _imgtrans.so -fPIC -O2 _imgtrans.c -Wall
+	gcc -shared -o _imgtrans.so -fPIC -Ofast _imgtrans.c -Wall
+
+test:	imgtrans.py _imgtrans.so
+	python imgtrans.py
+	python imgtrans.py /data/id11/nanoscope/Commissioning/bliss/labradorite3_z3_/dt_y0025_/interlaced_1_1/Frelon/interlaced_1_1_Frelon0123.edf /data/id11/jon/wood_cell/els_2_6_topo_/els_2_6_topo_0023.edf
+
--- imgtrans.py
+++ imgtrans.py
@@ -1,9 +1,9 @@
 
-
+from __future__ import print_function , division
 
 
 from ctypes import c_int, c_void_p, c_char, cdll
-import numpy as np
+import numpy as np, zlib
 
 _imgtrans = cdll.LoadLibrary("./_imgtrans.so")
 
@@ -19,30 +19,33 @@ _rebin3.argtypes = [ c_void_p, c_int, c_int, c_void_p ]
 _rebin4 = _imgtrans.rebin4
 _rebin4.argtypes = [ c_void_p, c_int, c_int, c_void_p ]
 
+
 LUT = ( c_char*65536 ).in_dll( _imgtrans, "LUT" )
 
 def rebin2(img, out):
-    assert out.shape[0] == img.shape[0]/2
-    assert out.shape[1] == img.shape[1]/2
+    assert out.shape[0] == img.shape[0]//2
+    assert out.shape[1] == img.shape[1]//2
     _rebin2(img.ctypes.data, img.shape[0], img.shape[1], out.ctypes.data)
 
 def rebin3(img, out):
-    assert out.shape[0] == img.shape[0]/3
-    assert out.shape[1] == img.shape[1]/3
+    assert out.shape[0] == img.shape[0]//3
+    assert out.shape[1] == img.shape[1]//3
     _rebin3(img.ctypes.data, img.shape[0], img.shape[1], out.ctypes.data)
 
 def rebin4(img, out):
-    assert out.shape[0] == img.shape[0]/4
-    assert out.shape[1] == img.shape[1]/4
+    assert out.shape[0] == img.shape[0]//4
+    assert out.shape[1] == img.shape[1]//4
     _rebin4(img.ctypes.data, img.shape[0], img.shape[1], out.ctypes.data)
-
+    
 LINEAR = 0
 LOG = 1
 SQRT = 2
 
 # initialise a default mapping for a Frelon camera at ID11
 setLUT( 90, 60000, LOG )
-        
+
+__all__ = [setLUT, LINEAR, LOG, SQRT, rebin2, rebin3, rebin4 ]
+
 def testLUT():
     from pylab import plot, show, legend
     setLUT( 90, 60000, LINEAR )
@@ -54,22 +57,55 @@ def testLUT():
     legend(loc='lower right')
     show()
 
+def compress( a ):
+    return a.shape,zlib.compress( a.tostring(), 1 )
+
+def decompress( o ):
+    return np.fromstring( zlib.decompress( o[1] ), np.uint8 ).reshape( o[0] )
+
 def showrebins(fname):
     import time
     im = fabio.open( fname )
-    r2 = np.zeros( (im.data.shape[0]/2,im.data.shape[1]/2), np.int8)
-    r3 = np.zeros( (im.data.shape[0]/3,im.data.shape[1]/3), np.int8)
-    r4 = np.zeros( (im.data.shape[0]/4,im.data.shape[1]/4), np.int8)
+    r2 = np.zeros( (im.data.shape[0]//2,im.data.shape[1]//2), np.uint8)
+    r3 = np.zeros( (im.data.shape[0]//3,im.data.shape[1]//3), np.uint8)
+    r4 = np.zeros( (im.data.shape[0]//4,im.data.shape[1]//4), np.uint8)
     start = time.time()
     rebin2( im.data, r2 )
     t2 = time.time()-start
+
+    start = time.time()
+    o2 = compress( r2 )
+    z2 = time.time()-start
+
     start = time.time()
     rebin3( im.data, r3 )
     t3 = time.time()-start
+
+    start = time.time()
+    o3 = compress( r3 )
+    z3 = time.time()-start
+    
     start = time.time()
     rebin4( im.data, r4 )
     t4 = time.time()-start
-    
+
+    start = time.time()
+    o4 = compress( r4 )
+    z4 = time.time()-start
+
+    assert (decompress( o2 ) == r2 ).all()
+    assert (decompress( o3 ) == r3 ).all()
+    assert (decompress( o4 ) == r4 ).all()
+
+    print( fname  )
+    total = im.data.nbytes
+    print( "2x : %.3f ms zlib: %.2f ms size %d kB gain %.1f vs. 8 "%(
+        1e3*t2, 1e3*z2, len(o2[1])//1024, total/len(o2[1])))
+    print( "3x : %.3f ms zlib: %.2f ms size %d kB gain %.1f vs. 18"%(
+        1e3*t3, 1e3*z3, len(o3[1])//1024, total/len(o3[1])))
+    print( "4x : %.3f ms zlib: %.2f ms size %d kB gain %.1f vs. 32"%(
+        1e3*t4, 1e3*z4, len(o4[1])//1024, total/len(o4[1])))
+
     from pylab import imshow, show, subplot, title
     import pylab
     pylab.spectral()
