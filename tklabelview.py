083d9f255e550d45004cd4c47ed32ff4aa946cca
Jon Wright <wright@esrf.fr>
2018-09-25 13:55:39 +0200

    py3 windoze

--- tklabelview.py
+++ tklabelview.py
@@ -1,8 +1,14 @@
 
-import Tkinter
+from __future__ import print_function, division
+
+import time, sys
+if sys.version_info[0] < 3:
+    import Tkinter as tk
+else:
+    import tkinter as tk
 from PIL import Image, ImageTk
 import numpy as np
-import time
+
 import colormaps
 
 class mainWindow():
@@ -14,11 +20,10 @@ class mainWindow():
     p = colormaps.viridis
     
     def __init__(self):
-        print "Delay",self.delay
-        self.root = Tkinter.Tk()
-        self.frame = Tkinter.Frame(self.root, width=512, height=512)
+        self.root = tk.Tk()
+        self.frame = tk.Frame(self.root, width=512, height=512)
         self.frame.pack()
-        self.label = Tkinter.Label( self.frame ) #, fill=Tkinter.BOTH )
+        self.label = tk.Label( self.frame ) 
         self.label.pack()
         self.root.after(self.delay, self.start) # INCREASE THE 0 TO SLOW IT DOWN
         self.root.mainloop()
@@ -28,7 +33,7 @@ class mainWindow():
     def start(self):
         global data
         if self.im is None:
-            self.im=Image.fromstring('P',
+            self.im=Image.frombuffer('P',
                                  (self.data.shape[1],
                                   self.data.shape[0]),
                                  self.data.astype('b').tostring() )
@@ -38,10 +43,11 @@ class mainWindow():
         else:
             self.im.putdata( self.data.astype('b').tostring() )
             self.photo.paste( self.im )
-        self.root.update()
+        self.root.update_idletasks()
         self.times+=1
         if self.times%33==0:
-            print "%.02f FPS"%(self.times/(time.time()-self.timestart))
+            print ( "%.02f FPS"%(self.times/(time.time()-self.timestart)),
+                    end="\r" )
         self.root.after(self.delay,self.start)
         self.data=np.roll(self.data,-1,1)
 
